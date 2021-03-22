a46e45b2bce96b25e4f7b67282809234725ae945
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-03-22 17:19:17 +0900

    AuthGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleawre } from './jwt/jwt.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
