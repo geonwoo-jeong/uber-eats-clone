d2886aa2be119def047123098e549b087dc1b6d1
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-03-26 23:42:44 +0900

    Mocking

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,14 +1,43 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
-    service = module.get<UsersService>(UsersService);
+    service = modules.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
